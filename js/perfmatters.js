5e9ecdc52f7a6eec079a0b85e20b009053ebc8cb
pratiklodha95 <lodhapratik9@gmail.com>
2014-10-09 01:10:56 +0530

    optimized

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,14 +1,1 @@
-// Measuring the Critical Rendering Path with Navigation Timing
-// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
-
-function logCRP() {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
-  var stats = document.getElementById("crp-stats");
-  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
-}
-
-window.addEventListener("load", function(event) {
-  logCRP();
-});
+function logCRP(){var d=window.performance.timing,b=d.domContentLoadedEventStart-d.domLoading,c=d.domComplete-d.domLoading;var a=document.getElementById("crp-stats");a.textContent="DCL: "+b+"ms, onload: "+c+"ms"}window.addEventListener("load",function(a){logCRP()});
